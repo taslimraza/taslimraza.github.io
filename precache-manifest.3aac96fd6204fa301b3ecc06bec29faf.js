56736a1331d877f4f44f5cf052a5c7d4ebfbc5d6
Taslim <taslim.raza@mobigesture.com>
2019-02-09 20:46:19 +0530

    react app

---
+++ precache-manifest.3aac96fd6204fa301b3ecc06bec29faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7cf313ab78fc787dff4e",
+    "url": "/static/css/main.0b7e5016.chunk.css"
+  },
+  {
+    "revision": "7cf313ab78fc787dff4e",
+    "url": "/static/js/main.7cf313ab.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e75b7621ac0da000c23bb2265e264f8e",
+    "url": "/index.html"
+  }
+];
